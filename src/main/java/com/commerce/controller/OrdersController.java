c421d05e945374837ad836b680a3e57be5b58913
THOMASGREG\jefferson.caceres <jefferson.caceres@thomasgreg.com>
2024-09-14 23:13:24 -0500

    feat: se crea controlador de productos y orders

--- src/main/java/com/commerce/controller/OrdersController.java
+++ src/main/java/com/commerce/controller/OrdersController.java
@@ -1,4 +1,53 @@
 package com.commerce.controller;
 
+import com.commerce.model.OrdersDto;
+import com.commerce.service.OrdersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/orders")
 public class OrdersController {
+    private final OrdersService ordersService;
+
+
+    public OrdersController(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<OrdersDto>> getAllOrders() {
+        List<OrdersDto> orders = ordersService.findAll();
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrdersDto> getOrderById(@PathVariable int id) {
+        OrdersDto order = ordersService.findById(id);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<OrdersDto> createOrder(@RequestBody OrdersDto ordersDto) {
+        OrdersDto createdOrder = ordersService.save(ordersDto);
+        return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrdersDto> updateOrder(@PathVariable int id, @RequestBody OrdersDto ordersDto) {
+        OrdersDto updatedOrder = ordersService.update(id, ordersDto);
+        return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrderById(@PathVariable int id) {
+        ordersService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/commerce/controller/ProductController.java
+++ src/main/java/com/commerce/controller/ProductController.java
@@ -1,4 +1,54 @@
 package com.commerce.controller;
 
+import com.commerce.model.ProductDto;
+import com.commerce.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    private final ProductService productService;
+
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        List<ProductDto> products = productService.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
+        ProductDto createdProduct = productService.save(productDto);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable int id) {
+        ProductDto product = productService.findId(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable int id, @RequestBody ProductDto productDto) {
+        ProductDto updatedProduct = productService.update(id, productDto);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable int id) {
+        productService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
