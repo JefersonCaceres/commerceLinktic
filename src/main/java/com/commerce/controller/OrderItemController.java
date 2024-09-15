cf634a773b6de9130e157f3eb393908078f668a6
THOMASGREG\jefferson.caceres <jefferson.caceres@thomasgreg.com>
2024-09-14 23:55:56 -0500

    feat: ajustes de controlador de order items

--- src/main/java/com/commerce/controller/OrderItemController.java
+++ src/main/java/com/commerce/controller/OrderItemController.java
@@ -3,6 +3,7 @@ package com.commerce.controller;
 import com.commerce.model.OrderItemDto;
 import com.commerce.service.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,15 +15,33 @@ public class OrderItemController {
     @Autowired
     private OrderItemService orderItemService;
 
-    @PostMapping
-    public ResponseEntity<OrderItemDto> createOrderItem(@RequestBody OrderItemDto orderItemDto) {
-        OrderItemDto savedOrderItem = orderItemService.save(orderItemDto);
-        return ResponseEntity.ok(savedOrderItem);
-    }
-
     @GetMapping
     public ResponseEntity<List<OrderItemDto>> getAllOrderItems() {
         List<OrderItemDto> orderItems = orderItemService.findAll();
-        return ResponseEntity.ok(orderItems);
+        return new ResponseEntity<>(orderItems, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItemDto> getOrderItemById(@PathVariable int id) {
+        OrderItemDto orderItem = orderItemService.findById(id);
+        return new ResponseEntity<>(orderItem, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderItemDto> createOrderItem(@RequestBody OrderItemDto orderItemDto) {
+        OrderItemDto createdOrderItem = orderItemService.save(orderItemDto);
+        return new ResponseEntity<>(createdOrderItem, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderItemDto> updateOrderItem(@PathVariable int id, @RequestBody OrderItemDto orderItemDto) {
+        OrderItemDto updatedOrderItem = orderItemService.update(id, orderItemDto);
+        return new ResponseEntity<>(updatedOrderItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrderItem(@PathVariable int id) {
+        orderItemService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
